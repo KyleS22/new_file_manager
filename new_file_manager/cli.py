78b12d85b41678854cab57cb697126519ff5be89
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2019-01-29 21:22:50 -0600

    Fixed positional argument problem

--- new_file_manager/cli.py
+++ new_file_manager/cli.py
@@ -9,7 +9,7 @@ from new_file_manager.new_file_manager import MAIN_DIR
 def main():
     parser = argparse.ArgumentParser(description="Create a new file with a header and templates")
 
-    parser.add_argument("file_name", help="The name of the new file including extension")
+    parser.add_argument("file_name", nargs='?', default=None, help="The name of the new file including extension")
     parser.add_argument("--header", help="The name of the header to use")
     parser.add_argument("--list_headers", action="store_true", help="List header names")
     parser.add_argument("--template", help="The name of a template to use")
@@ -17,27 +17,30 @@ def main():
 
     args = parser.parse_args()
 
-    header_string = ""
-    file_path, filetype = os.path.splitext(args.file_name)
-
-    # Display help if there are not enough arguments
-    if len(sys.argv) < 1:
+    if len(sys.argv) < 2:
         parser.print_help()
-
-    if args.header:
-        header = nfm.load_header(args.header)
-        comment_char = nfm.get_comment_char(filetype)
-        header_string = nfm.get_header_string(header, comment_char)
 
     if args.list_headers:
         nfm.list_headers()
         sys.exit(0)
 
 
-
     if args.list_templates:
         nfm.list_templates()
         sys.exit(0)
+
+
+    if args.file_name is None:
+        sys.exit(0)
+
+    header_string = ""
+    file_path, filetype = os.path.splitext(args.file_name)
+
+    if args.header:
+        header = nfm.load_header(args.header)
+        comment_char = nfm.get_comment_char(filetype)
+        header_string = nfm.get_header_string(header, comment_char)
+
 
 
     nfm.set_up()
